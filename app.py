2cd577ac07b2fd2b85955767e17b72fcc63f4e94
zhongyunshu <zhongyunshu2021@163.com>
2023-05-21 18:21:38 +0800

    Update README.md

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, request
+
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -8,5 +9,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', "POST"])  # 路由默认接收请求方式位POST，然而登录所需要请求都有，所以要特别声明。
+def login():
+    if request.method == 'POST':
+        if request.form['password'] == '1234':
+            return render_template('test.html')
+        else:
+            return 'wrong'
+    elif request.method == 'GET':
+        return redirect(url_for('index'))
+
+
+@app.route('/')
+def test():
+    return render_template('test.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8181)
