81c91c76f92ac5a27c2e16a28ef262e0d66a30df
CloudClock-zhongyunshu <134182486+cloudclock-zhongyunshu@users.noreply.github.com>
2023-05-21 12:25:26 +0800

    Update README.md

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def test():  # put application's code here
-    return 'test,failed!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
